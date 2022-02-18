4961bb5e42e389a64c2cf1d533af5df21ddc7b31
AndrewMilovich <triplle@ukr.net>
2022-02-18 17:19:28 +0200

    done all homework and practic

--- lesson2/app.js
+++ lesson2/app.js
@@ -2,27 +2,91 @@ const express = require('express');
 const path = require("path");
 const {engine} = require('express-handlebars')
 
-
 const app = express();
 app.use(express.json())
-app.use( express.urlencoded({extends:true}))
+app.use(express.urlencoded({extends: true}))
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
-// app.get('/', (req, res) => {
-//     req.send('Welcome')
-// })
+const users = [];
 
-app.get('/user',(req,res)=>{
-   res.render('user');
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
-
-app.use((req,res)=>{
-    res.render('errorPage')
+app.get('/user', (req, res) => {
+    res.render('user');
 })
+
+app.get('/users', (req, res) => {
+    const {age, city} = req.query;
+
+    let filter = [...users];
+
+    if (age) {
+        filter = filter.filter(value => value.age === age)
+    }
+    if (city) {
+        filter = filter.filter(value => value.city === city)
+    }
+
+    res.render('users', {filter});
+})
+
+app.get('/users/:userId', (req, res) => {
+
+    const {userId} = req.params;
+    const user = users[userId - 1];
+
+    res.render('user', {user})
+})
+
+app.post('/login', (req, res) => {
+
+    const filterEmail = users.find(value => value.email === req.body.email)
+
+    if (filterEmail) {
+        res.redirect('/errorPage');
+        return;
+    }
+
+    users.push({...req.body, id: users.length + 1});
+    res.redirect('/users')
+})
+
+app.post('/delete/:userId', (req, res) => {
+    const {userId} = req.params;
+    users.splice(+userId - 1, 1);
+    res.redirect('/users');
+})
+
+app.get('/errorPage', (req, res) => {
+    res.render('errorPage');
+})
+
+app.get('/signIn', (req, res) => {
+    res.render('signIn');
+})
+
+app.post('/signIn', (req, res) => {
+
+    let filter = [...users];
+    filter = users.filter(value => value.email === req.body.email && value.password === req.body.password)
+
+    if (filter) {
+        res.render('users', {filter});
+        return
+    }
+
+    res.redirect('notFoundUser');
+})
+
+app.use((req, res) => {
+    res.render('errorPage');
+})
+
 app.listen(5000, () => {
 })
